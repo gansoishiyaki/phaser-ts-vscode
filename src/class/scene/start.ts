605789b8609d031669369acdbdb1de7b7d314e3c
gansoishiyaki <gansoishiyaki@gmail.com>
2021-01-29 15:39:58 +0900

    fix launch

--- src/class/scene/start.ts
+++ src/class/scene/start.ts
@@ -1,5 +1,5 @@
 export class StartScene extends Phaser.Scene {
   create() {
-    let text = this.add.text(0, 0, 'Phaser 3').setFontSize(32).setColor('#ff0');
+    let text = this.add.text(0, 0, 'いえあ').setFontSize(32).setColor('#ff0');
   }
 }
